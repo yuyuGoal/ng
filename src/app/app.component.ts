891692f10ed7f9e948bfde6c1baf8c7b4c4326f7
GJLMoTea <gjlmotea@gmail.com>
2023-11-12 23:08:17 +0800

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'ng';
