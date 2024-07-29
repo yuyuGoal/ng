366dea0aca0cc57650be1bb0d63858716399ffc6
gjlmotea <gjlmotea@gmail.com>
2024-07-30 03:17:51 +0800

    add syncfusion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { EventSettingsModel, DayService, WeekService, WorkWeekService, MonthService, AgendaService, ScheduleModule } from '@syncfusion/ej2-angular-schedule';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ButtonModule],
+  providers: [DayService, WeekService, WorkWeekService, MonthService, AgendaService],
+  imports: [CommonModule, RouterOutlet, ButtonModule, ScheduleModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+  }
+  public data: object[] = [
+    {
+      Id: 1,
+      Subject: 'Meeting',
+      StartTime: new Date(2023, 1, 15, 10, 0),
+      EndTime: new Date(2023, 1, 15, 12, 30)
+    },
+  ];
+  public selectedDate: Date = new Date(2023, 1, 15);
+  public eventSettings: EventSettingsModel = {
+    dataSource: this.data
+  };
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { AgendaService, DayService, MonthAgendaService, MonthService, TimelineMonthService, TimelineViewsService, WeekService, WorkWeekService } from '@syncfusion/ej2-angular-schedule';
 
 import { routes } from './app.routes';
+import { GridModule } from '@syncfusion/ej2-angular-grids';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), GridModule, DayService, WeekService, WorkWeekService, MonthService, AgendaService, MonthAgendaService, TimelineViewsService, TimelineMonthService]
 };
